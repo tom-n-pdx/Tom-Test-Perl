d39168c5c76ad695dec8d4221d30a58c2d640a82
Tom Shott <tom.n.pdx@gmail.com>
2018-09-20 15:25:03 -0700

    Finished Chapter 8

---
+++ Book-Object_Oriented_Perl-Fenwick/Chapter08-Class_Methods/Test-PlayingCard.pl
@@ -0,0 +1,27 @@
+#
+# Program to test playing cards class
+#
+
+
+use strict;
+use warnings;
+
+use lib '.';
+use PlayingCard;
+
+my $card = PlayingCard->new("Ace", "Spades");
+print "Suit is: ", $card->get_suit, "\n";
+print "Card Count: ", PlayingCard->card_count, "\n";
+
+my $card = PlayingCard->new("King", "Spades");
+print "Suit is: ", $card->get_suit, "\n";
+print "Card Count: ", PlayingCard->card_count, "\n";
+
+
+# Invoke Class method
+my @deck = PlayingCard->new_deck();
+print("First Card in Deck: ", $deck[0], "\n");
+
+print "Card Count: ", PlayingCard->card_count, "\n";
+ 
+
