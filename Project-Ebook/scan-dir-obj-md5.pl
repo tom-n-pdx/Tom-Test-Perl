5682690e07b04318a8a721891e83ab0a08fb06ae
Tom Shott <tom.n.pdx@gmail.com>
2019-02-09 12:34:57 -0800

    Updated md5 scan code

--- Project-Ebook/scan-dir-obj-md5.pl
+++ Project-Ebook/scan-dir-obj-md5.pl
@@ -14,11 +14,11 @@ use MooFile;
 #
 # Todo
 # * add --help option
-# * Add status update if doing lots of md5 on dir
 # * move collect dupes, into seperate sub
 # * merge ave tree & save dir into one sub
-# * check dupes when scan unchanged dir
-# * bug - if no dir change - won't update missing md5 if fast scan earrlier
+# * write check dupes sub
+# * write update file sub
+
 
 # dupes
 # * problem is when first dupe in seperate dir from 2nd dupe - need to redo first dir
@@ -28,6 +28,9 @@ use MooFile;
 my %Files;
 my %size_count;
 my %size_dir;
+
+my ($files_new, $files_rename, $files_delete, $files_change) = (0, 0, 0, 0);
+
 
 our $debug = 0;
 our $verbose = 1;
@@ -93,9 +96,10 @@ sub wanted {
 }
 
 
+
 #
 # Scan a dir and calc md5 values. As part of scan will check if dir valid, and load and save a md5 db file
-#
+# * BUG - updates dir - but does not save
 sub update_dir_oop {
     my %opt = @_;
     my $fast_scan =  delete $opt{fast_scan} // 0;
@@ -104,13 +108,12 @@ sub update_dir_oop {
 
     say "\tFAST SCAN" if ($fast_scan && $verbose >= 2);
     my %Files;
+    ($files_new, $files_rename, $files_delete, $files_change) = (0, 0, 0, 0);
 
     if (!-e $dir_check or !-d $dir_check or !-r $dir_check){
     	warn "Bad Dir: $dir_check";
     	return %Files;
     }
-
-    my ($files_new, $files_rename, $files_delete, $files_change) = (0, 0, 0, 0);
 
     say "Scanning $dir_check" if ($verbose >= 1);
     my $Dir = MooDir->new(filepath => $dir_check);
@@ -118,58 +121,31 @@ sub update_dir_oop {
     my ($Files_old_ref, $db_mtime) = load_dir_oop_db(dir => $dir_check);
     my %Files_old = %{$Files_old_ref};
 
-    if ($db_mtime >= $Dir->dtime){
-	say "No Files chaged, skip update" if ($verbose >= 2);
-	return(%Files_old);
-    }
+
+    # if ($db_mtime >= $Dir->dtime){
+    # 	say "No Files chaged, skip update" if ($verbose >= 2);
+    # 	return(%Files_old);
+    # }
+
 
     my @filepaths = $Dir->list_filepaths();
     say "Files: ", join(", ", @filepaths) if ($verbose >= 3);
 
     # Scan through for all files in dir
     foreach my $filepath (@filepaths) {
-	my $File = MooFile->new(filepath => $filepath, opt_update_md5=>0);
+	my $File = update_file(filepath=>$filepath, Files_old_ref=>\%Files_old, fast_scan=>$fast_scan);
 	my $hash = $File->inode;
-
-	my $File_old = $Files_old{$hash};
-	if (! defined $File_old) {
-	    # Assume if no old file with inode, must be new file
-	    say "New File: ", $File->filename if ($verbose >= 1);
-	    $files_new++;
-	} else {
-	    # If old file newer or same age, transfer md5
-	    if (defined $File_old->md5 && $File->size == $File_old->size && $File->mtime <= $File_old->mtime) {
-		$File->_set_md5($File_old->md5);
-	    }
-	    # Check if name changed
-	    if ($File->filename ne $File_old->filename) {
-		say "Rename: ", $File_old->filename, " to ", $File->filename if ($verbose >= 1);
-		$files_rename++;
-	    }
-	}
-	delete $Files_old{$hash};
-
-
-	$size_count{$File->size}++;
-	my $count = $size_count{$File->size} // 0;
-	# say "Count: $count" if ($count >= 2);
-	if ( !defined($File->md5) && ( !$fast_scan or $count >= 2)) {
-	    say "Calc md5: ", $File->filename if ($verbose >= 2 or $count >= 2);
-	    if ($File->isreadable){
-		$File->update_md5;
-		$files_change++;
-	    }
-	}
 	$Files{$hash} = $File;
 
-	if ($files_change > 1 && $files_change % 100 == 0) {
+	# Save every so often
+	if ($files_change > 0 && $files_change % 100 == 0){
 	    save_dir_oop_db(dir => $dir_check, Files_ref => \%Files);
-	    print "Dir: $dir_check " if ($verbose <= 1);
-	    say "File Changes: New: $files_new Rename: $files_rename Changed: $files_change Deleted: $files_delete";
+	    say "Checkpoint Save File Changes: New: $files_new Rename: $files_rename Changed: $files_change Deleted: $files_delete";
 	}
     }
 
-    # Now Check if any old values left
+
+    # Now Check if any old values left - file was deleted
     my @keys = keys %Files_old;
     $files_delete += scalar(@keys);
     if (@keys >= 1 && $verbose >= 1){
@@ -192,7 +168,7 @@ sub update_dir_oop {
 
 #
 # Debug - print list files
-# 
+# * Merge tree and dir save
 sub print_list_Files {
     my $Files_ref = shift(@_);;
 
@@ -232,6 +208,7 @@ sub save_tree_oop_db {
     my $dbfile = "$dir/.moo.obj.tree.db";
 
     store($Files_ref, $dbfile);
+    say "Saved ", scalar(keys %{$Files_ref}), " records" if ($verbose >= 2);
 
     return;
 }
@@ -255,4 +232,51 @@ sub load_dir_oop_db {
     }
 
     return ($Files_ref, $dbfile_mtime);
+}
+ 
+sub update_file {
+    my %opt = @_;
+
+    my $filepath =  delete $opt{filepath} or die "Missing param 'filepath' to update_file";
+    my $Files_old_ref = delete $opt{Files_old_ref} or die "Missing param 'Files_old_ref' to update_file";
+    my $fast_scan =  delete $opt{fast_scan} // 0;
+    die "Unknown params:", join ", ", keys %opt if %opt;
+
+    my $File = MooFile->new(filepath => $filepath, opt_update_md5=>0);
+    my $hash = $File->inode;
+
+    my $File_old = $$Files_old_ref{$hash};
+    if (! defined $File_old) {
+	# Assume if no old file with inode, must be new file
+	say "New File: ", $File->filename if ($verbose >= 1);
+	$files_new++;
+    } else {
+	# If old file newer or same age, transfer md5
+	if (defined $File_old->md5 && $File->size == $File_old->size && $File->mtime <= $File_old->mtime) {
+	    $File->_set_md5($File_old->md5);
+	}
+	# Check if name changed
+	if ($File->filename ne $File_old->filename) {
+	    say "Rename: ", $File_old->filename, " to ", $File->filename if ($verbose >= 1);
+	    $files_rename++;
+	}
+    }
+
+    delete $$Files_old_ref{$hash};
+
+    $size_count{$File->size}++;
+
+    if ( !defined($File->md5) ){
+	my $count = $size_count{$File->size};
+	if ( !$fast_scan or $count >= 2) {	    
+	    print "Dupe Count: $count- " if ($count >=2);
+	    say "Calc md5: ", $File->filename if ($verbose >= 2 or $count >= 2);
+
+	    if ($File->isreadable){
+		$File->update_md5;
+		$files_change++;
+	    }
+	}
+    }
+    return ($File);
 }
